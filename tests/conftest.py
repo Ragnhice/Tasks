8312b153ad6f193b41de4e4c4a03c1716626d553
Tatiana Ivanova <Tatiana.Ivanova@sitronics-kt.ru>
2022-12-28 17:00:09 +0300

    added check duration hook

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,23 @@ import pytest
 import inspect
 
 
-@pytest.fixture(scope="session", autouse=True)
+
+@pytest.hookspec(firstresult=True)
+def pytest_runtest_call(item):                # хук pytest_runtest_call - Вызывается для запуска теста
+    global start_time
+    start_time = datetime.datetime.now()
+
+                                     #location = (filename, lineno, testname)
+def pytest_runtest_logfinish(nodeid, location, request):            # хук Вызывается в конце выполнения протокола runtest
+    stop_time = datetime.datetime.now()
+    test_duration = (stop_time - start_time).total_seconds()
+    if test_duration > 7:
+        print(f"Длительность теста {request.node.name} {test_duration} секунд первышает 7 секунд")
+
+
+
+
+@pytest.fixture(scope="function", autouse=True)
 def check_duration(request):
     """
         Выводит название и время выполнения теста, полное (вместе с
@@ -13,14 +29,19 @@ def check_duration(request):
 
     """
 
+   # разобраться с хуками в пайтесте
+   #Тест в пайтесте делится на 3 части: 1 - запуск всех фикстур, 2 - запуск теста, 3 - завершение всех фикстур
+   #  поэтому делать фикстуру для измерения времени длительности всего запуска теста (вместе с фикстурами) некорректно.
     start_time = datetime.datetime.now()
     yield
     stop_time = datetime.datetime.now()
-    this_duration = (stop_time - start_time).total_seconds()
-    assert this_duration > 7, f"Длительность теста {request.node.name} первышает 7 секунд"
+    test_duration = (stop_time - start_time).total_seconds()
+    assert test_duration > 7, f"Длительность теста {request.node.name} {test_duration} секунд первышает 7 секунд,"
 
 
-@pytest.fixture(scope="session", autouse=True)    # как применить фикстуру не к тесту, а ко всем модулям в проекте?
+
+#генератор тестов, а не фикстура
+#@pytest.fixture(scope="session", autouse=True)    # как применить фикстуру не к тесту, а ко всем модулям в проекте?
 def check_documentation(module_name):             # как передавать имя модулей?
     """
     Проверяет наличие документации у методов классов по имени модуля
